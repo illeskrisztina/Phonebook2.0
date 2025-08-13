8c568e54e97663db570b34ae15228d15548b1bae
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-13 09:44:09 +0200

    Formatted classes and replaced ArrayList to List interface = ArrayList

--- Phonebook/src/main/java/application/main/Database/DAOs/PersonDAO.java
+++ Phonebook/src/main/java/application/main/Database/DAOs/PersonDAO.java
@@ -8,138 +8,113 @@ import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
-public class PersonDAO extends DatabaseHandlerFactory implements IPersonDAO
-{
-  private static PersonDAO instance;
+public class PersonDAO extends DatabaseHandlerFactory implements IPersonDAO {
+    private static PersonDAO instance;
 
-  private PersonDAO() throws SQLException
-  {
-    DriverManager.registerDriver(new SQLServerDriver());
-  }
-
-  public static synchronized PersonDAO getInstance()
-  {
-    try
-    {
-      if (instance == null)
-      {
-        instance = new PersonDAO();
-      }
-      return instance;
+    private PersonDAO() throws SQLException {
+        DriverManager.registerDriver(new SQLServerDriver());
     }
-    catch (SQLException e)
-    {
-      throw new RuntimeException("Issue getting singleton instance of PersonDAO: " + e.getMessage());
-    }
-  }
-  @Override public synchronized Person createPerson(Person person)
-  {
-    try(Connection connection = super.establishConnection())
-    {
-      PreparedStatement statement = connection.prepareStatement("insert into phonebook.people(name, age) values (?, ?);", Statement.RETURN_GENERATED_KEYS);
-      statement.setString(1, person.getName());
-      statement.setInt(2, person.getAge());
-      statement.executeUpdate();
 
-      ResultSet generatedKeys = statement.getGeneratedKeys();
-      if(generatedKeys.next())
-      {
-        person.setId(generatedKeys.getInt(1));
-      }
-      else
-      {
-        throw new RuntimeException("No keys were generated.");
-      }
-      return person;
+    public static synchronized PersonDAO getInstance() {
+        try {
+            if (instance == null) {
+                instance = new PersonDAO();
+            }
+            return instance;
+        } catch (SQLException e) {
+            throw new RuntimeException("Issue getting singleton instance of PersonDAO: " + e.getMessage());
+        }
     }
-    catch (SQLException e)
-    {
-      throw new RuntimeException("Something went wrong while creating an entry for a person in the database: " + e.getMessage());
+
+    @Override
+    public synchronized Person createPerson(Person person) {
+        try (Connection connection = super.establishConnection()) {
+            PreparedStatement statement = connection.prepareStatement("insert into phonebook.people(name, age) values (?, ?);", Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, person.getName());
+            statement.setInt(2, person.getAge());
+            statement.executeUpdate();
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                person.setId(generatedKeys.getInt(1));
+            } else {
+                throw new RuntimeException("No keys were generated.");
+            }
+            return person;
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong while creating an entry for a person in the database: " + e.getMessage());
+        }
     }
-  }
 
-  @Override public SimplePersonDTO getPerson(int Id)
-  {
-    try(Connection connection = super.establishConnection())
-    {
-      PreparedStatement statement = connection.prepareStatement("SELECT people.name, people.age, people.id\n"
-          + "FROM phonebook.people\n" + "WHERE people.id = ?;");
-      statement.setInt(1, Id);
-      ResultSet rs = statement.executeQuery();
+    @Override
+    public SimplePersonDTO getPerson(int Id) {
+        try (Connection connection = super.establishConnection()) {
+            PreparedStatement statement = connection.prepareStatement("SELECT people.name, people.age, people.id\n"
+                    + "FROM phonebook.people\n" + "WHERE people.id = ?;");
+            statement.setInt(1, Id);
+            ResultSet rs = statement.executeQuery();
 
-      SimplePersonDTO fetched = null;
-      while (rs.next())
-      {
-        fetched = new SimplePersonDTO(rs.getString("name"),
-            rs.getInt("age"), rs.getInt("id"));
-      }
+            SimplePersonDTO fetched = null;
+            while (rs.next()) {
+                fetched = new SimplePersonDTO(rs.getString("name"),
+                        rs.getInt("age"), rs.getInt("id"));
+            }
 
-      return fetched;
+            return fetched;
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong while fetching the data of the person with id " + Id + ": " + e.getMessage());
+        }
     }
-    catch (SQLException e)
-    {
-      throw new RuntimeException("Something went wrong while fetching the data of the person with id " + Id + ": " + e.getMessage());
-    }
-  }
 
-  @Override public ArrayList<SimplePersonDTO> getAllPeople()
-  {
-    try(Connection connection = super.establishConnection())
-    {
-      ArrayList<SimplePersonDTO> allPeople = new ArrayList<>();
+    @Override
+    public List<SimplePersonDTO> getAllPeople() {
+        try (Connection connection = super.establishConnection()) {
+            List<SimplePersonDTO> allPeople = new ArrayList<>();
 
-      PreparedStatement statement = connection.prepareStatement("SELECT people.name, people.age, people.id\n"
-          + "FROM phonebook.people;");
-      ResultSet rs = statement.executeQuery();
-      while (rs.next())
-      {
-        String name = rs.getString("name");
-        int age = rs.getInt("age");
-        int Id = rs.getInt("id");
-        allPeople.add(new SimplePersonDTO(name, age, Id));
-      }
+            PreparedStatement statement = connection.prepareStatement("SELECT people.name, people.age, people.id\n"
+                    + "FROM phonebook.people;");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                int Id = rs.getInt("id");
+                allPeople.add(new SimplePersonDTO(name, age, Id));
+            }
 
-      return allPeople;
+            return allPeople;
+        } catch (SQLException e) {
+            throw new RuntimeException("Could not connect to database while retrieving information for people stored in the database: " + e.getMessage());
+        }
     }
-    catch (SQLException e)
-    {
-      throw new RuntimeException("Could not connect to database while retrieving information for people stored in the database: " + e.getMessage());
-    }
-  }
 
-  @Override public synchronized Person updatePerson(Person person)
-  {
-    try(Connection connection = super.establishConnection())
-    {
-      PreparedStatement statement = connection.prepareStatement("update phonebook.people set name = ?, age = ? where id = ?;");
-      statement.setString(1, person.getName());
-      statement.setInt(2, person.getAge());
-      statement.setInt(3, person.getId());
-      statement.executeUpdate();
+    @Override
+    public synchronized Person updatePerson(Person person) {
+        try (Connection connection = super.establishConnection()) {
+            PreparedStatement statement = connection.prepareStatement("update phonebook.people set name = ?, age = ? where id = ?;");
+            statement.setString(1, person.getName());
+            statement.setInt(2, person.getAge());
+            statement.setInt(3, person.getId());
+            statement.executeUpdate();
 
-      return person;
+            return person;
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong while trying to update the information of a person with id " + person.getId() + ": " + e.getMessage());
+        }
     }
-    catch (SQLException e)
-    {
-      throw new RuntimeException("Something went wrong while trying to update the information of a person with id " + person.getId() + ": " + e.getMessage());
-    }
-  }
 
-  @Override public synchronized SimplePersonDTO deletePerson(int Id)
-  {
-    try(Connection connection = super.establishConnection())
-    {
-      SimplePersonDTO deleted = getPerson(Id);
-      PreparedStatement statement = connection.prepareStatement("delete from phonebook.people where id = ?;");
-      statement.setInt(1, Id);
-      statement.executeUpdate();
+    @Override
+    public synchronized SimplePersonDTO deletePerson(int Id) {
+        try (Connection connection = super.establishConnection()) {
+            SimplePersonDTO deleted = getPerson(Id);
+            PreparedStatement statement = connection.prepareStatement("delete from phonebook.people where id = ?;");
+            statement.setInt(1, Id);
+            statement.executeUpdate();
 
-      return deleted;
+            return deleted;
+        } catch (SQLException e) {
+            throw new RuntimeException("Something went wrong while deleting person with id " + Id + " from the database: " + e.getMessage());
+        }
     }
-    catch (SQLException e)
-    {
-      throw new RuntimeException("Something went wrong while deleting person with id " + Id + " from the database: " + e.getMessage());
-    }
-  }
 }
--- Phonebook/src/main/java/application/main/Database/Interfaces/IPersonDAO.java
+++ Phonebook/src/main/java/application/main/Database/Interfaces/IPersonDAO.java
@@ -3,13 +3,16 @@ package application.main.Database.Interfaces;
 import application.main.Entities.DTOs.SimplePersonDTO;
 import application.main.Entities.Person;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public interface IPersonDAO
-{
-  public Person createPerson(Person person);
-  public SimplePersonDTO getPerson(int Id);
-  public ArrayList<SimplePersonDTO> getAllPeople();
-  public Person updatePerson(Person person);
-  public SimplePersonDTO deletePerson(int Id);
+public interface IPersonDAO {
+    public Person createPerson(Person person);
+
+    public SimplePersonDTO getPerson(int Id);
+
+    public List<SimplePersonDTO> getAllPeople();
+
+    public Person updatePerson(Person person);
+
+    public SimplePersonDTO deletePerson(int Id);
 }
