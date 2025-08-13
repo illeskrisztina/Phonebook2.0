50e75c010b2455f9dfb81db0b70e6d6b1e71fd38
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-13 15:23:21 +0200

    AddressService created, this most likely will cause errors as refactoring is needed -> take everything apart, AddressDAO has no place in PersonService

--- Phonebook/src/main/java/application/main/Services/Interfaces/IAddressService.java
+++ Phonebook/src/main/java/application/main/Services/Interfaces/IAddressService.java
@@ -5,11 +5,11 @@ import application.main.Entities.Address;
 import java.util.List;
 
 public interface IAddressService {
-    public Address createAddress(Address address);
+    public Address createAddress(int personId, Address address);
 
     public Address getAddress(int id);
 
-    public List<Address> getAllAddress();
+    public List<Address> getAllAddress(Integer personId);
 
     public Address updateAddress(Address address);
 
