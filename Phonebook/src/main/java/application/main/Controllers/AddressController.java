bcd81c9f40d5dc07a12bcc4e26e0c47bbf41d6fc
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-13 14:17:26 +0200

    Skeleton of AddressController.java filled with TODOs created

---
+++ Phonebook/src/main/java/application/main/Controllers/AddressController.java
@@ -0,0 +1,42 @@
+package application.main.Controllers;
+
+import application.main.Entities.Address;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AddressController {
+
+    @PostMapping("/people/{personId}/addresses")
+    public ResponseEntity<Address> addAddress(@PathVariable("personId") int personId, @RequestBody Address address) {
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/people/{personId}/addresses/{addressId}")
+    public ResponseEntity<Address> getAddress(@PathVariable("personId") int personId, @PathVariable("addressId") int addressId) {
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/addresses")
+    public ResponseEntity<List<Address>> getAddresses(@RequestParam(name = "personId", required = false) Integer personId) {
+        //TODO
+        return null;
+    }
+
+    @PutMapping("/addresses")
+    public ResponseEntity<Address> updateAddress(@RequestBody Address address) {
+        //TODO
+        return null;
+    }
+
+    @DeleteMapping("/addresses/{addressId}")
+    public ResponseEntity<Address> deleteAddress(@PathVariable("addressId") int addressId) {
+        //TODO
+        return null;
+    }
+}
