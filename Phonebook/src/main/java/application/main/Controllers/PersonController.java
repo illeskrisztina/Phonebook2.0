746af10f5ebc2b5f7859f6f1167a2fea2f53860f
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-14 10:59:37 +0200

    PersonController fixed to fit new Model object

--- Phonebook/src/main/java/application/main/Controllers/PersonController.java
+++ Phonebook/src/main/java/application/main/Controllers/PersonController.java
@@ -3,6 +3,8 @@ package application.main.Controllers;
 import application.main.Entities.Address;
 import application.main.Entities.DTOs.SimplePersonDTO;
 import application.main.Entities.Person;
+import application.main.Model.Interfaces.IModel;
+import application.main.Model.Model;
 import application.main.Services.Interfaces.IPersonService;
 import application.main.Services.PersonService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,13 +21,14 @@ import java.util.NoSuchElementException;
 @RequestMapping("/api/people")
 public class PersonController
 {
-  IPersonService personService = new PersonService();
+  IModel dispatcher = new Model();
+
   @PostMapping
   public ResponseEntity<Person> createPerson(@RequestBody Person person)
   {
     try
     {
-      Person created = personService.createPerson(person);
+      Person created = dispatcher.createPerson(person);
       return new ResponseEntity<>(person, HttpStatus.CREATED);
     }
     catch (Exception e)
@@ -40,7 +43,7 @@ public class PersonController
   {
     try
     {
-      SimplePersonDTO person = personService.getPerson(id);
+      SimplePersonDTO person = dispatcher.getPerson(id);
       return new ResponseEntity<>(person, HttpStatus.OK);
     }
     catch (NoSuchElementException f)
@@ -60,7 +63,7 @@ public class PersonController
   {
     try
     {
-      List<SimplePersonDTO> allPeople = personService.getAllPeople();
+      List<SimplePersonDTO> allPeople = dispatcher.getAllPeople();
       return new ResponseEntity<>(allPeople, HttpStatus.OK);
     }
     catch (Exception e)
@@ -75,7 +78,7 @@ public class PersonController
   {
     try
     {
-      Person updated = personService.updatePerson(person);
+      Person updated = dispatcher.updatePerson(person);
       return new ResponseEntity<>(updated, HttpStatus.OK);
     }
     catch (NoSuchElementException f)
@@ -95,7 +98,7 @@ public class PersonController
   {
     try
     {
-      Person deleted = personService.deletePerson(id);
+      Person deleted = dispatcher.deletePerson(id);
       return new ResponseEntity<>(deleted, HttpStatus.OK);
     }
     catch (Exception e)
