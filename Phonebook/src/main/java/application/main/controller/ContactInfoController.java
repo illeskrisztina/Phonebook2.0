42206665dc19779765bf84c51056ba19e049b664
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-22 13:32:53 +0200

    Endpoint for getting one and many contact info created

---
+++ Phonebook/src/main/java/application/main/controller/ContactInfoController.java
@@ -0,0 +1,37 @@
+package application.main.controller;
+
+import application.main.model.Entity.ContactInfo;
+import application.main.service.Dispatcher;
+import application.main.service.Interfaces.IDispatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class ContactInfoController {
+    private final IDispatcher dispatcher = new Dispatcher();
+
+    @PostMapping("/person/{personId}/contact")
+    public ResponseEntity<ContactInfo> addContactInfo(@PathVariable int personId, @RequestBody ContactInfo contactInfo) {
+        ContactInfo created = dispatcher.addContact(contactInfo, personId);
+        if(created == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/contact/{contactId}")
+    public ResponseEntity<ContactInfo> getContactInfo(@PathVariable String contactId) {
+        ContactInfo fetched = dispatcher.getContact(contactId);
+        if(fetched == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(fetched, HttpStatus.OK);
+    }
+
+    @GetMapping("/contact")
+    public ResponseEntity<List<ContactInfo>> getAllContactInfo(@RequestParam(name = "addressId", required = false) Integer addressId) {
+        return new ResponseEntity<>(dispatcher.getAllContacts(addressId),  HttpStatus.OK);
+    }
+}
