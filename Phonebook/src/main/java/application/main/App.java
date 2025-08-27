e3335271d82440354dcd3c9f0907037a6da6fbeb
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-27 10:20:32 +0200

    Method to execute after the application's start added

--- Phonebook/src/main/java/application/main/App.java
+++ Phonebook/src/main/java/application/main/App.java
@@ -1,13 +1,17 @@
 package application.main;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App
-{
-  public static void main(String[] args)
-  {
-    SpringApplication.run(App.class, args);
-  }
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @PostConstruct
+    public void readJsonFiles() {
+
+    }
 }
