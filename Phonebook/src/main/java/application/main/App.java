4b0fabdac7074cedd59d4b541f9e78654fe3570d
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-27 11:54:30 +0200

    Method for reading an initial file and saving the contents in the database started

--- Phonebook/src/main/java/application/main/App.java
+++ Phonebook/src/main/java/application/main/App.java
@@ -1,8 +1,14 @@
 package application.main;
 
+import application.main.model.Entity.ContactInfo;
+import application.main.service.Dispatcher;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class App {
@@ -12,6 +18,17 @@ public class App {
 
     @PostConstruct
     public void readJsonFiles() {
+        ObjectMapper mapper = new ObjectMapper();
+        Dispatcher dispatcher = new Dispatcher();
 
+        try{
+            ContactInfo contact = mapper.readValue(new File(System.getProperty("user.dir") + "/Phonebook/src/main/resources/contact.json"), ContactInfo.class);
+
+            //Contact and address needs to be separated
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
