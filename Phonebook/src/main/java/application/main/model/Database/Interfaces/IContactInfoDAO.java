c548bd8fe3131b764fae5f062f5399f836c745d0
illeskrisztina <illes.krisztina@kesmarki.com>
2025-08-28 07:38:49 +0200

    Separated te method of adding a contact info into the database into two methods

--- Phonebook/src/main/java/application/main/model/Database/Interfaces/IContactInfoDAO.java
+++ Phonebook/src/main/java/application/main/model/Database/Interfaces/IContactInfoDAO.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 
 public interface IContactInfoDAO
 {
-  public ContactInfo createContactInfo(ContactInfo contactInfo, int addressId);
+  public ContactInfo createContactInfo(ContactInfo contactInfo);
+  public void addContactInfoToAddress(String contactInfo, int addressId);
   public ContactInfo getContactInfo(String contact);
   public ArrayList<ContactInfo> getAllContactInfo();
   public ArrayList<ContactInfo> getAllContactInfoForAddress(int addressId);
