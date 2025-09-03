aedc829153e1724ce3862efa13aba1cc41e83c2e
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-03 09:59:29 +0200

    Replaced return values
    Optimised and added headers to ResponseEntity return values in AddressController

--- Phonebook/src/main/java/application/main/controller/AddressController.java
+++ Phonebook/src/main/java/application/main/controller/AddressController.java
@@ -3,6 +3,7 @@ package application.main.controller;
 import application.main.model.entity.Address;
 import application.main.service.Dispatcher;
 import application.main.service.interfaces.IDispatcher;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,45 +14,61 @@ import java.util.List;
 @RequestMapping("/api")
 public class AddressController {
     private IDispatcher dispatcher = new Dispatcher();
+    private static final String ERROR_HEADER = "Error";
 
     @PostMapping("/people/{personId}/addresses")
     public ResponseEntity<Address> addAddress(@PathVariable("personId") int personId, @RequestBody Address address) {
         Address created = dispatcher.createAddress(personId, address);
         if (created == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .header(ERROR_HEADER, "Entity could not be created.")
+                    .build();
         }
-        return new ResponseEntity<>(created, HttpStatus.CREATED);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header(HttpHeaders.LOCATION, "addresses/" + created.getAddressId())
+                .body(created);
     }
 
     @GetMapping("addresses/{addressId}")
     public ResponseEntity<Address> getAddress(@PathVariable("addressId") int addressId) {
         Address fetched = dispatcher.getAddress(addressId);
         if (fetched == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .header(ERROR_HEADER,"Address cannot be found.")
+                    .build();
         }
-        return new ResponseEntity<>(fetched, HttpStatus.FOUND);
+        return ResponseEntity.ok(fetched);
     }
 
     @GetMapping("/addresses")
     public ResponseEntity<List<Address>> getAddresses(@RequestParam(name = "personId", required = false) Integer personId) {
-        return new ResponseEntity<>(dispatcher.getAllAddress(personId), HttpStatus.OK);
+        return ResponseEntity.ok(dispatcher.getAllAddress(personId));
     }
 
     @PutMapping("/addresses")
     public ResponseEntity<Address> updateAddress(@RequestBody Address address) {
         Address updated = dispatcher.updateAddress(address);
         if (updated == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .header(ERROR_HEADER, "Address could not be updated.")
+                    .build();
         }
-        return new ResponseEntity<>(updated, HttpStatus.OK);
+        return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/addresses/{addressId}")
     public ResponseEntity<Address> deleteAddress(@PathVariable("addressId") int addressId) {
         Address deleted = dispatcher.deleteAddress(addressId);
         if (deleted == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .header(ERROR_HEADER, "Address could not be deleted.")
+                    .build();
         }
-        return new ResponseEntity<>(deleted, HttpStatus.OK);
+        return ResponseEntity.ok(deleted);
     }
 }
