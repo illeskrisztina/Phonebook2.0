ef12e2b92dfb2fdbb2d7728d0b782c8eb45c62dc
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-08 15:45:35 +0200

    Exception change
    Un-beaned DatabaseConnectionException

--- Phonebook/src/main/java/application/main/model/exception/DatabaseConnectionException.java
+++ Phonebook/src/main/java/application/main/model/exception/DatabaseConnectionException.java
@@ -1,8 +1,6 @@
 package application.main.model.exception;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class DatabaseConnectionException extends RuntimeException {
     public DatabaseConnectionException(String message) {
         super(message);
