57c0c2d3957706613be01cd5f57d32c1dad4a0b4
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-08 16:28:04 +0200

    Post-construct data injection
    Changed the main class to be able to insert a single contact info into the database

--- Phonebook/src/main/java/application/main/App.java
+++ Phonebook/src/main/java/application/main/App.java
@@ -4,6 +4,7 @@ import application.main.model.entity.ContactInfo;
 import application.main.service.Dispatcher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,17 +14,18 @@ import java.io.File;
 import java.io.IOException;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class App {
-    Logger logger = LoggerFactory.getLogger(App.class);
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final Dispatcher dispatcher;
+    private final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @PostConstruct
     public void readJsonFiles() {
-        ObjectMapper mapper = new ObjectMapper();
-        Dispatcher dispatcher = new Dispatcher();
-
         try{
             ContactInfo contact = mapper.readValue(new File(System.getProperty("user.dir") + "/Phonebook/src/main/resources/contact.json"), ContactInfo.class);
 
