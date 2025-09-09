ce65100bade3ad8fab6b61fc9c292d1d9fce0615
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-09 13:31:55 +0200

    PersonService modified
    Modified PersonService to work with new implementation of PersonDAO

--- Phonebook/src/main/java/application/main/service/PersonService.java
+++ Phonebook/src/main/java/application/main/service/PersonService.java
@@ -1,27 +1,30 @@
 package application.main.service;
 
 import application.main.model.database.dao.PersonDAO;
-import application.main.model.database.interfaces.IPersonDAO;
-import application.main.model.entity.dto.SimplePersonDTO;
 import application.main.model.entity.Person;
+import application.main.model.entity.dto.PersonMapper;
+import application.main.model.entity.dto.SimplePersonDTO;
 import application.main.service.interfaces.IPersonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService implements IPersonService {
-    private final IPersonDAO personDAO = PersonDAO.getInstance();
+    private final PersonDAO personDAO;
+    private final PersonMapper personMapper;
 
     @Override
     public Person createPerson(Person person) {
-        return personDAO.createPerson(person);
+        return personDAO.save(person);
     }
 
     @Override
     public SimplePersonDTO getPerson(int id) {
-        SimplePersonDTO person = personDAO.getPerson(id);
+        SimplePersonDTO person = personMapper.personToSimplePersonDTO(personDAO.findById(id).orElse(null));
 
         if (person == null) {
             throw new NoSuchElementException("The person under id " + id + " does not exist");
@@ -32,22 +35,22 @@ public class PersonService implements IPersonService {
 
     @Override
     public List<SimplePersonDTO> getAllPeople() {
-        return personDAO.getAllPeople();
+        return personDAO.findAll().stream().map(personMapper::personToSimplePersonDTO).toList();
     }
 
     @Override
     public Person updatePerson(Person person) {
-        Person updated = personDAO.updatePerson(person);
-
-        if (updated == null) {
-            throw new NoSuchElementException("The person under id " + person.getId() + " does not exist");
-        }
-
-        return updated;
+        return personDAO.save(person);
     }
 
     @Override
     public Person deletePerson(int id) {
-        return personDAO.deletePerson(id);
+        Person deleted = personDAO.findById(id).orElse(null);
+
+        if (deleted != null) {
+            personDAO.deleteById(id);
+        }
+
+        return deleted;
     }
 }
