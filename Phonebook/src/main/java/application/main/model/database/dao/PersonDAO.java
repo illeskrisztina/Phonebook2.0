72f58babd8322e72c82f32c0a098324a83e71d7e
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-11 14:15:50 +0200

    Modified PersonDAO
    Added @Repository to PersonDAO

--- Phonebook/src/main/java/application/main/model/database/dao/PersonDAO.java
+++ Phonebook/src/main/java/application/main/model/database/dao/PersonDAO.java
@@ -2,8 +2,10 @@ package application.main.model.database.dao;
 
 import application.main.model.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface PersonDAO extends JpaRepository<Person, Integer> {
 
 }
