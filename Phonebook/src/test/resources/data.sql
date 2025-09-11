79e4713ce4be8507340cf5288b1775d2ef359e39
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-11 14:05:27 +0200

    Test data added
    Test data added to test resources

--- Phonebook/src/test/resources/data.sql
+++ Phonebook/src/test/resources/data.sql
@@ -1,17 +1,16 @@
-insert into phonebook.person(name, age) values ('Jane Doe', 24);
-insert into phonebook.person(name, age) values ('Mary Hopkins', 67);
-insert into phonebook.person(name, age) values ('David Jones', 16);
+insert into phonebook.address(address, type) values ('Hungary, Budapest, 1194, Some street 13.', 'permanent');
+insert into phonebook.address(address, type) values ('Jozefa Kozaceka 2068/29, 960 01 Zvolen, Slovakia', 'permanent');
+insert into phonebook.address(address, type) values ('Hungary, Balatonboglar, Rozsa u. 37, 8630', 'temporary');
+insert into phonebook.address(address, type) values ('Aby Baekgardsvej 14, 8230 Aarhus, Denmark', 'permanent');
+
+insert into phonebook.person(name, age, permanent_id) values ('Jane Doe', 24, 1);
+insert into phonebook.person(name, age, permanent_id, temporary_id) values ('Mary Hopkins', 67, 2, 3);
+insert into phonebook.person(name, age, permanent_id) values ('David Jones', 16, 4);
 
 
-insert into phonebook.address(address, person_id, type) values ('Hungary, Budapest, 1194, Some street 13.', 1, 'permanent');
-insert into phonebook.address(address, person_id, type) values ('Jozefa Kozaceka 2068/29, 960 01 Zvolen, Slovakia', 2, 'permanent');
-insert into phonebook.address(address, person_id, type) values ('Hungary, Balatonboglar, Rozsa u. 37, 8630', 2, 'temporary');
-insert into phonebook.address(address, person_id, type) values ('Aby Baekgardsvej 14, 8230 Aarhus, Denmark', 3, 'permanent');
-
-
-insert into phonebook.contact_info(type, contact) values ('mobile phone', '+36 20 234 5678')
-insert into phonebook.contact_info(type, contact) values ('mobile phone', '+36 30 555 6666')
-insert into phonebook.contact_info(type, contact) values ('e-mail', 'davy_blue@gmail.com')
+insert into phonebook.contact_info(type, contact) values ('mobile phone', '+36 20 234 5678');
+insert into phonebook.contact_info(type, contact) values ('mobile phone', '+36 30 555 6666');
+insert into phonebook.contact_info(type, contact) values ('e-mail', 'davy_blue@gmail.com');
 
 
 insert into phonebook.address_contacts(contact, address_id) values ('+36 20 234 5678', 1);
