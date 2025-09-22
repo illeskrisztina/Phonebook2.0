5099c5f17f05f4f2ad919f4e3231d47e832a42e2
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-22 16:13:41 +0200

    ContactInfoTest separated
    Separated controller method's tests

--- Phonebook/src/test/java/application/main/controller/ContactInfoControllerTest.java
+++ Phonebook/src/test/java/application/main/controller/ContactInfoControllerTest.java
@@ -1,4 +1,4 @@
-package application.main;
+package application.main.controller;
 
 import application.main.model.entity.Address;
 import application.main.model.entity.ContactInfo;
@@ -18,7 +18,7 @@ import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class ContactInfoTest {
+class ContactInfoControllerTest {
     ContactInfo contactTest;
     @Autowired
     private TestRestTemplate restTemplate;
@@ -28,53 +28,6 @@ class ContactInfoTest {
         contactTest = new ContactInfo()
                 .setType("mobile phone")
                 .setContact("+36 1 234 5678");
-    }
-
-    @Test
-    void created_object_not_null() {
-        Assertions.assertNotNull(contactTest);
-    }
-
-    @Test
-    void created_object_has_correct_contact_type() {
-        Assertions.assertEquals("mobile phone", contactTest.getType());
-    }
-
-    @Test
-    void created_object_has_correct_contact() {
-        Assertions.assertEquals("+36 1 234 5678", contactTest.getContact());
-    }
-
-    @Test
-    void created_object_type_can_be_set() {
-        contactTest.setType("email");
-
-        Assertions.assertEquals("email", contactTest.getType());
-    }
-
-    @Test
-    void created_object_contact_can_be_set() {
-        contactTest.setContact("hi@gmail.com");
-
-        Assertions.assertEquals("hi@gmail.com", contactTest.getContact());
-    }
-
-    @Test
-    void different_objects_same_attributes_are_equal() {
-        ContactInfo other = new ContactInfo()
-                .setType("mobile phone")
-                .setContact("+36 1 234 5678");
-
-        Assertions.assertEquals(true, other.equals(contactTest));
-    }
-
-    @Test
-    void different_objects_different_attributes_not_equal() {
-        ContactInfo other = new ContactInfo()
-                .setType("email")
-                .setContact("hi@gmail.com");
-
-        Assertions.assertEquals(false, other.equals(contactTest));
     }
 
     @Test
