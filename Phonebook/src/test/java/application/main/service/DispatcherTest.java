e5745be1411b7c987b71027734d17af8ea1a3512
illeskrisztina <illes.krisztina@kesmarki.com>
2025-09-22 13:38:49 +0200

    Dispatcher tested
    Address section of dispatcher tested

--- Phonebook/src/test/java/application/main/service/DispatcherTest.java
+++ Phonebook/src/test/java/application/main/service/DispatcherTest.java
@@ -4,6 +4,7 @@ import application.main.model.entity.Address;
 import application.main.model.entity.ContactInfo;
 import application.main.model.entity.Person;
 import application.main.model.entity.dto.PersonMapper;
+import application.main.model.entity.dto.SimplePersonDTO;
 import application.main.model.entity.enums.AddressType;
 import application.main.model.exception.NoSuchAddressTypeException;
 import org.junit.jupiter.api.Assertions;
@@ -156,4 +157,100 @@ class DispatcherTest {
 
         Assertions.assertThrows(NoSuchAddressTypeException.class, () -> dispatcher.deletePerson(1));
     }
+
+    @Test
+    void creating_address_sets_person_address_appropriate_to_type() {
+        personTest.setId(1);
+
+        when(personService.getPerson(1)).thenReturn(personTest);
+        when(addressService.createAddress(permanentAddressTest)).thenReturn(permanentAddressTest.setId(1));
+        when(addressService.createAddress(temporaryAddressTest)).thenReturn(temporaryAddressTest.setId(2));
+        when(personMapper.personToSimplePersonDTO(personTest)).thenReturn(
+                new SimplePersonDTO()
+                        .setId(1)
+                        .setName(personTest.getName())
+                        .setAge(personTest.getAge())
+        );
+
+        dispatcher.createAddress(1, permanentAddressTest);
+        dispatcher.createAddress(1, temporaryAddressTest);
+
+        verify(personService, times(1)).updatePerson(personTest.setTemporaryAddress(temporaryAddressTest));
+
+        personTest.setTemporaryAddress(null);
+
+        verify(personService, times(1)).updatePerson(personTest.setPermanentAddress(permanentAddressTest));
+    }
+
+    @Test
+    void creating_address_with_invalid_address_type_throws_error() {
+        personTest.setId(1);
+
+        when(personService.getPerson(1)).thenReturn(personTest);
+        when(addressService.createAddress(permanentAddressTest.setType("invalid"))).thenReturn(permanentAddressTest.setId(1));
+        when(personMapper.personToSimplePersonDTO(personTest)).thenReturn(
+                new SimplePersonDTO()
+                        .setId(1)
+                        .setName(personTest.getName())
+                        .setAge(personTest.getAge())
+        );
+
+        Assertions.assertThrows(NoSuchAddressTypeException.class, () -> dispatcher.createAddress(1, permanentAddressTest));
+    }
+
+    @Test
+    void getAddress_calls_appropriate_service_method() {
+        when(addressService.getAddress(1)).thenReturn(permanentAddressTest);
+
+        dispatcher.getAddress(1);
+
+        verify(addressService, times(1)).getAddress(1);
+    }
+
+    @Test
+    void if_no_id_is_given_all_addresses_are_called() {
+        when(addressService.getAllAddress()).thenReturn(List.of(permanentAddressTest, temporaryAddressTest));
+
+        dispatcher.getAllAddress(null);
+
+        verify(addressService, times(1)).getAllAddress();
+    }
+
+    @Test
+    void person_with_both_addresses_returns_list_of_size_two() {
+        when(personService.getPerson(1))
+                .thenReturn(personTest
+                        .setTemporaryAddress(temporaryAddressTest)
+                        .setPermanentAddress(permanentAddressTest));
+
+        List<Address> addresses = dispatcher.getAllAddress(1);
+
+        Assertions.assertEquals(2, addresses.size());
+    }
+
+    @Test
+    void updateAddress_calls_appropriate_service_method() {
+        when(addressService.updateAddress(temporaryAddressTest)).thenReturn(temporaryAddressTest);
+
+        dispatcher.updateAddress(temporaryAddressTest);
+
+        verify(addressService, times(1)).updateAddress(temporaryAddressTest);
+    }
+
+    @Test
+    void deleting_address_updates_person_addresses() {
+        personTest.setTemporaryAddress(temporaryAddressTest);
+        personTest.setPermanentAddress(permanentAddressTest);
+
+        lenient().when(personService.getAllPeople()).thenReturn(List.of(personTest));
+        lenient().when(addressService.getAddress(1)).thenReturn(permanentAddressTest);
+        lenient().when(addressService.getAddress(2)).thenReturn(temporaryAddressTest);
+
+        dispatcher.deleteAddress(1);
+        dispatcher.deleteAddress(2);
+
+        verify(personService, times(2)).updatePerson(personTest);
+    }
+
+
 }
